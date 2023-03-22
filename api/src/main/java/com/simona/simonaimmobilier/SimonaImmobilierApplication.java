1ca8def50a10382f2e745a05e2a4d8d1e1aef08c
xingcdev <60323726+xingcdev@users.noreply.github.com>
2023-03-22 22:04:01 +0100

    initialize api project

---
+++ api/src/main/java/com/simona/simonaimmobilier/SimonaImmobilierApplication.java
@@ -0,0 +1,13 @@
+package com.simona.simonaimmobilier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimonaImmobilierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimonaImmobilierApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/simona/simonaimmobilier/SimonaImmobilierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simona.simonaimmobilier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimonaImmobilierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
